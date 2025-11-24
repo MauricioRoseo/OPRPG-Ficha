26cf4e6053de244737ae73ad4ef3fa56c0d14326
MauricioRoseo <mauriciornn@gmail.com>
2025-11-24 19:17:39 -0300

    Organização de diretorio

--- api-backend-ficha/src/controllers/item/getItemController.js
+++
@@ -1,8 +0,0 @@
-import {getList} from '../../models/itemModel.js'
- 
- export default async function getItemController(req, res) {
-     
-     const result = await getList()
-     
-     return res.json(result)
- }
